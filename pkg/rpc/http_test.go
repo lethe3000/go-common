480009274b5452d8d9dbcc0667656babf4c23001
lethe <lethe>
2021-04-08 11:18:05 +0800

    test: add test

--- pkg/rpc/http_test.go
+++ pkg/rpc/http_test.go
@@ -7,9 +7,38 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	testUrl     = "http://inf-feishu-app-manager.nt.dev.fiture.com/api/v1/corps/1"
+	valid       = "Y6bKZsE1gVn6thYsEIDIerD-oYrNvCwFUGg0NSapy0M"
+	invalid     = "1"
+	validSecret = map[string]string{
+		"secret": valid,
+	}
+	invalidSecret = map[string]string{
+		"secret": invalid,
+	}
+)
+
 func TestGet(t *testing.T) {
-	var resp map[string]interface{}
-	err := Get("http://inf-feishu-app-manager.nt.dev.fiture.com/api/v1/corps/1", nil, map[string]string{"secret": "1"}, &resp)
-	assert.NotNil(t, err)
-	assert.True(t, errors.Is(err, HttpStatusCodeBadErr))
+	t.Run("valid", func(t *testing.T) {
+		var resp map[string]interface{}
+		err := Get(testUrl, validSecret, validSecret, &resp)
+		assert.Nil(t, err)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		var resp map[string]interface{}
+		err := Get(testUrl, nil, invalidSecret, &resp)
+		assert.NotNil(t, err)
+		assert.True(t, errors.Is(err, HttpStatusCodeBadErr))
+	})
+
+}
+
+func TestPost(t *testing.T) {
+	t.Run("error", func(t *testing.T) {
+		var resp map[string]interface{}
+		err := Post(testUrl, map[string]interface{}{}, nil, nil, &resp)
+		assert.NotNil(t, err)
+	})
 }
